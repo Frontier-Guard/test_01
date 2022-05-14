fc02e40a79485b5fe0bb2d59f82fde772322e938
Alex Mulyavko <Shurik.slav@gmail.com>
2022-05-14 17:25:31 +0300

    First component

---
+++ src/components/ToxaProfileCard.js
@@ -0,0 +1,14 @@
+import './styles.css';
+
+export default function ToxaProfileCard(props) {
+    return (
+        <div className="Somecomponent">
+            <div>
+                <img src='http://placekitten.com/200/300' alt="logo"/>
+            </div>
+            <div>
+                Ткаченко Антон Вячеславович
+            </div>
+        </div>
+    );
+}
